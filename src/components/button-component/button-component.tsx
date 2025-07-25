de6cf7bd1afba319aee45534313f8ad06ca8b3ee
Laercio Nogueira <laercio.nogueira@rw3tecnologia.com>
2025-07-25 00:47:26 -0300

    fix button

--- src/components/button-component/button-component.tsx
+++ src/components/button-component/button-component.tsx
@@ -1,8 +1,9 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h, Element, Listen } from '@stencil/core';
 
 @Component({
   tag: 'button-component',
   styleUrl: 'button-component.scss',
+  // scoped: true,
   shadow: true,
 })
 export class ButtonComponent {
@@ -23,6 +24,30 @@ export class ButtonComponent {
   @Prop() startIcon: Node;
   @Prop() sx: object;
   @Prop() variant: 'contained' | 'outlined' | 'text' = 'text';
+
+  @Element() host: HTMLElement;
+
+  @Listen('click', { capture: true })
+  handleClick(event: MouseEvent) {
+    const button = this.host.shadowRoot.querySelector('button');
+    const ripple = document.createElement('span');
+    ripple.classList.add('ripple');
+
+    const rect = button.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    ripple.style.width = ripple.style.height = `${size}px`;
+
+    const x = event.clientX - rect.left - size / 2;
+    const y = event.clientY - rect.top - size / 2;
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    button.appendChild(ripple);
+
+    ripple.addEventListener('animationend', () => {
+      ripple.remove();
+    });
+  }
 
   render() {
     return (
